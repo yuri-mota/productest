fa56794479fabf32315c1693942c0fc2e814587e
Yuri <yuri.mota@outlook.com.br>
2023-05-21 17:48:34 -0300

    Commit inicial

---
+++ src/main/java/com/productest/productest/ProductestApplication.java
@@ -0,0 +1,13 @@
+package com.productest.productest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/productest/productest/ProductestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productest.productest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
